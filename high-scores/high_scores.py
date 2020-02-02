1f085596196b52b2f9521b565c9d2466878b18fa
AQuaZar <greenskullcompany@gmail.com>
2020-02-02 16:27:17 +0200

    high-scores[2]

--- high-scores/high_scores.py
+++ high-scores/high_scores.py
@@ -7,12 +7,5 @@ def personal_best(scores):
 
 
 def personal_top_three(scores):
-    result = []
-    if len(scores) >= 3:
-        size = 3
-    else:
-        size = len(scores)
-    for _ in range(size):
-        result.append(max(scores))
-        scores.remove(max(scores))
-    return result
+    return sorted(scores, reverse=True)[:3]
+
